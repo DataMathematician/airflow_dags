4931cb305c237fec2478b1d94e5d2ef3dd0a2345
George Volkov <datamathematician@gmail.com>
2022-11-01 10:22:01 +0300

    fix

--- dag_update.py
+++ dag_update.py
@@ -8,15 +8,22 @@ dag = DAG(
     dag_id="dag_puller",
     default_args={"depends_on_past":False},
     start_date=datetime.datetime(2022,1,1),
-    schedule_interval="*/5 2 * * *",
+    schedule_interval="*/5 * * * *",
     catchup=False,
     )
 
 fetch_code = BashOperator(
     task_id="fetch_code",
     bash_command=(
-        "cd /airflow/dags &&"
+        "apt-get update && "
+        "apt-get install -y git && "
+        "git config --global user.name 'TestAirflowUser' && "
+        "git config --global user.email 'Bloodjanp@gmail.com' && "
+        "cd /opt/airflow/dags && "
+        #"git init && "
+        #"git remote add origin https://github.com/DataMathematician/airflow_dags.git && "
         "git reset --hard origin/master"
+        "echo ++++++++++++++++++++++++++++++++++++++++++ "
     ),
     dag=dag
 )
